9a818f4f5ce52f4e929081e9c6c31a6855503d3f
Md. Hasanur Rahman <hasanurhasu121@gmail.com>
2024-02-13 02:08:52 +0600

    fix admin problem

--- index.js
+++ index.js
@@ -46,17 +46,16 @@ async function run() {
       res.send({ token })
     })
 
-    // middleware
-
+    // middlewares 
     const verifyToken = (req, res, next) => {
-      // console.log('inside verify token', req.headers);
+      // console.log('inside verify token', req.headers.authorization);
       if (!req.headers.authorization) {
-        return res.status(401).send({ message: 'forbidden access' })
+        return res.status(401).send({ message: 'unauthorized access' });
       }
       const token = req.headers.authorization.split(' ')[1];
       jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
         if (err) {
-          return res.status(401).send({ message: 'forbidden access' });
+          return res.status(401).send({ message: 'unauthorized access' })
         }
         req.decoded = decoded;
         next();
@@ -75,7 +74,7 @@ async function run() {
       next();
     }
 
-    app.get('/users/admin/:email', verifyToken, async (req, res) => {
+    app.get('/admin/:email', verifyToken, async (req, res) => {
       const email = req.params.email;
 
       if (email !== req.decoded.email) {
@@ -91,27 +90,8 @@ async function run() {
       res.send({ admin });
     })
 
-
-    // app.get('/users/admin/:email', verifyToken, async (req, res) => {
-    //   const email = req.params.email;
-    //   console.log(email);
-
-    //   if (email !== req.decoded.email) {
-    //     return res.status(403).send({ message: 'forbidden access' })
-    //   }
-
-    //   const query = { email: email };
-    //   const user = await userCollection.findOne(query);
-    //   console.log(user);
-    //   let admin = false;
-    //   if (user) {
-    //     admin = user?.role === 'admin';
-    //   }
-    //   res.send({ admin });
-    // })
-
     // get all user from the database
-    app.get('/users', verifyToken, async (req, res) => {
+    app.get('/users', verifyToken, verifyAdmin,  async (req, res) => {
       console.log(req.headers);
       const users = userCollection.find();
       const result = await users.toArray();
